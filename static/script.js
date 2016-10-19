d08a6aa2ab8861d8f6230a671e20748d38a8a22d
ahmdrz <ahmadrezazibaei@hotmail.com>
2016-10-19 10:04:45 +0330

    low quality

--- static/script.js
+++ static/script.js
@@ -9,11 +9,8 @@ function changeColor() {
     document.getElementById("heading").setAttribute("style", "text-shadow:0px 0px 10px " + getRandomColor() + ";"), setTimeout(changeColor, 1e3)
 }
 
-setTimeout(function () {
-    setTimeout(changeColor, 1e3);    
-}, 2e5);
-
 function start() {
+    setTimeout(changeColor, 1e3);  
     document.getElementById("overlay").style.opacity = "0";
     document.getElementById("preloader").style.display = "none";
     document.getElementById("content").style.display = "block";

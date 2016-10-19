478b3e07ec3b77792ab0d34ab0e0abf8794f9b3b
ahmdrz <ahmadrezazibaei@hotmail.com>
2016-10-19 09:56:18 +0330

    preloader

--- static/script.js
+++ static/script.js
@@ -1,3 +1,5 @@
+window.onload = function() {setTimeout(start,1e3);}
+
 function getRandomColor() {
     for (var a = "0123456789ABCDEF", b = "#", c = 0; c < 6; c++) b += a[Math.floor(16 * Math.random())];
     return b
@@ -7,6 +9,11 @@ function changeColor() {
     document.getElementById("heading").setAttribute("style", "text-shadow:0px 0px 10px " + getRandomColor() + ";"), setTimeout(changeColor, 1e3)
 }
 
-setTimeout(function() {
-    setTimeout(changeColor, 1e3);
-}, 2e3);
+setTimeout(function () {
+    setTimeout(changeColor, 1e3);    
+}, 2e5);
+
+function start() {
+    document.getElementById("preloader").style.display = "none";
+    document.getElementById("content").style.display = "block";
+}
